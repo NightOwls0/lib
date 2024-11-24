3b0f6dc893896c394426c6ab99d998cdc99a371a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2024-11-24 11:45:28 +0430

    update

---
+++ .changeset/six-bottles-change.md
@@ -0,0 +1,5 @@
+---
+'owls-lib': patch
+---
+
+Export missing components
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,12 @@
 // Reexport your entry components here
 //
+export {default as Badge} from './components/Badge.svelte'
+export {default as Alert} from './components/Alert.svelte'
 export {default as Button} from './components/Button.svelte'
 export {default as Tabs} from './components/Tabs/Tabs.svelte'
 export {default as TabPanel} from './components/Tabs/TabPanel.svelte'
 export {default as TabItem} from './components/Tabs/TabItem.svelte'
 export {default as TabHeader} from './components/Tabs/TabHeader.svelte'
 export {default as TabContent} from './components/Tabs/TabContent.svelte'
+export {default as Modal} from './components/Modal/Modal.svelte'
+export {default as ModalContent} from './components/Modal/ModalContent.svelte'
